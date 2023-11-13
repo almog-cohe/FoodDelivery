bb3128a0feccbbba4241c49c2ce3ba00abdef694
almog-cohe <almogqwerty@gmail.com>
2023-11-13 19:11:35 +0200

    adding an input field

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,70 +1,120 @@
-import Colors from '@/constants/Colors'
-import { Ionicons } from '@expo/vector-icons'
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SearchBar = () => (
+  <View style={styles.searchContainer}>
+    <View style={styles.searchSection}>
+      <View style={styles.searchField}>
+        <Ionicons style={styles.searchIcon} name="ios-search" size={20} color={Colors.medium} />
+        <TextInput style={styles.input} placeholder="Restaurants, groceries, dishes" />
+      </View>
+      <Link href={"/"} asChild>
+        <TouchableOpacity style={styles.optionButton}>
+          <Ionicons name="options-outline" size={30} color={Colors.primary} />
+        </TouchableOpacity>
+      </Link>
+    </View>
+  </View>
+);
+
 const CustomHeader = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <TouchableOpacity>
-          <Image style={styles.bike} source={require('@/assets/images/bike.png')} />
+          <Image
+            style={styles.bike}
+            source={require("@/assets/images/bike.png")}
+          />
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.titleContainer}>
           <Text style={styles.title}>Delivery • now</Text>
           <View style={styles.locationName}>
             <Text style={styles.subtitle}>London</Text>
-            <Ionicons name='chevron-down' size={22} color={Colors.primary} />
+            <Ionicons name="chevron-down" size={22} color={Colors.primary} />
           </View>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.profileButton}>
-          <Ionicons name='person-outline' size={28} color={Colors.primary} />
+          <Ionicons name="person-outline" size={28} color={Colors.primary} />
         </TouchableOpacity>
       </View>
+      <SearchBar />
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   safeArea: {
-    flex:1,
-    backgroundColor: 'white'
+    flex: 1,
+    backgroundColor: "white",
   },
   container: {
     height: 60,
-    backgroundColor: 'white',
-    flexDirection: 'row',
+    backgroundColor: "white",
+    flexDirection: "row",
     gap: 20,
-    justifyContent: 'space-between',
-    paddingHorizontal:20,
-    marginTop:19
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    marginTop: 19,
   },
   bike: {
     width: 60,
     height: 60,
-    backgroundColor:'#F0F6FB',
-    borderRadius: 30
+    backgroundColor: "#F0F6FB",
+    borderRadius: 30,
   },
   titleContainer: {
-    flex:1
+    flex: 1,
   },
   title: {
     fontSize: 14,
-    color: Colors.medium
+    color: Colors.medium,
   },
-  subtitle:{
+  subtitle: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   locationName: {
-    flexDirection: 'row', alignItems: 'center'
+    flexDirection: "row",
+    alignItems: "center",
   },
   profileButton: {
     backgroundColor: Colors.lightGrey,
     padding: 14,
-    borderRadius: 28
+    borderRadius: 28,
+  },
+  searchContainer: {
+    height: 60,
+    backgroundColor: "white",
+  },
+  searchSection: {
+    flexDirection: 'row',
+    gap:10,
+    paddingHorizontal:20,
+    alignItems: 'center'
+  },
+  searchField: {
+    flex:1,
+    backgroundColor: Colors.lightGrey,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  searchIcon: {
+    paddingLeft:10
+  },
+  input: {
+    padding:10,
+    color: Colors.mediumDark
+  },
+  optionButton: {
+    padding: 10,
+    borderRadius:8
   }
-})
+});
 
-export default CustomHeader
+export default CustomHeader;
